5562003b5feafc313870d6d4d938c83bed66afcd
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2014-10-09 13:52:23 +0300

    update package and tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 var gulp = require('gulp'),
-    watch = require('gulp-watch'),
     jade = require('gulp-jade'),
     uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
-    //sass = require('gulp-ruby-sass'),
     prefix = require('gulp-autoprefixer'),
     csso = require('gulp-csso'),
-    //imagemin = require('gulp-imagemin'),
+    imagemin = require('gulp-imagemin'),
     plumber = require('gulp-plumber'),
     browserify = require('browserify'),
     concat = require('gulp-concat'),
@@ -20,12 +18,18 @@ var gulp = require('gulp'),
     buffer = require('gulp-buffer'),
     size = require('gulp-size'),
     fs = require('fs'),
+    crypto = require('crypto'),
+    _ = require('lodash'),
+
+    webserver = require('gulp-webserver'),
 
     karma = require('gulp-karma'),
     source = require('vinyl-source-stream'),
     glob = require('glob'),
-    through = require('through'),
     coffee = require('coffee-script'),
+
+    duration = require('gulp-duration'),
+    argv = require('yargs').argv,
 
     runSequence = require('run-sequence'),
     livereload = require('gulp-livereload'),
@@ -34,78 +38,79 @@ var gulp = require('gulp'),
 var DEVELOPMENT = 'development',
     PRODUCTION = 'production',
     USE_FINGERPRINTING = false,
-    SASS_ENVIROMENT = "ruby",
-    USE_SASS_MAPS = true,
     BUILD = "builds/",
     ASSETS = "/assets",
     MOCKUPS = "_mockups",
     SRC = "_src",
+    useServer = false,
     TEST = "test",
+    watching = false,
     libs = [
-      './node_modules/jquery/dist/jquery.min.js',
-      './node_modules/underscore/underscore-min.js',
-      './node_modules/bootstrapify/lib/bootstrapify.js',
-      './node_modules/select2/select2.js',
-      './node_modules/bootstrap-datepicker/js/bootstrap-datepicker.js',
       './node_modules/angular/angular.min.js',
-    ],
-    ignoreLibs = [
-      'jquery',
-      'underscore',
-      'bootstrapify',
-      'select2/select2',
-      'bootstrap-datepicker/js/bootstrap-datepicker',
-      'angular/angular'
+      './node_modules/angular-route/angular-route.min.js',
+      './node_modules/underscore/underscore-min.js',
+      './node_modules/jquery/dist/jquery.min.js'
     ];
 
 var env = process.env.NODE_ENV || DEVELOPMENT;
 if (env!==DEVELOPMENT) env = PRODUCTION;
 
+var jadeFiles = argv.jade || '*';
+
 function getOutputDir() {
   return BUILD+env;
 }
+
 gulp.task('jade', function() {
-  var config = { "production": env == PRODUCTION }
+  var config = {
+    "production": env === PRODUCTION,
+    "pretty": env === DEVELOPMENT,
+    "locals": {}
+  };
 
   var jsManifest      = env === PRODUCTION && USE_FINGERPRINTING ? (JSON.parse(fs.readFileSync("./"+BUILD+'/rev/js/rev-manifest.json', "utf8"))) : {},
-      //vendorManifest  = env === PRODUCTION && USE_FINGERPRINTING ? (JSON.parse(fs.readFileSync("./"+BUILD+'/rev/js-vendor/rev-manifest.json', "utf8"))) : {},
-      cssManifest     = env === PRODUCTION && USE_FINGERPRINTING ? (JSON.parse(fs.readFileSync("./"+BUILD+'/rev/css/rev-manifest.json', "utf8"))) : {},
+      //vendorManifest  = env === PRODUCTION ? (JSON.parse(fs.readFileSync("./"+BUILD+'/rev/js-vendor/rev-manifest.json', "utf8"))) : {},
+      cssManifest     = env === PRODUCTION && USE_FINGERPRINTING? (JSON.parse(fs.readFileSync("./"+BUILD+'/rev/css/rev-manifest.json', "utf8"))) : {},
       imagesManifest  = env === PRODUCTION && USE_FINGERPRINTING ? (JSON.parse(fs.readFileSync("./"+BUILD+'/rev/images/rev-manifest.json', "utf8"))) : {};
 
-  if (env === DEVELOPMENT) {
-    config.pretty = true;
-  }
-  gulp.src(SRC+"/templates/*.jade")
+  gulp.src(SRC+"/templates/"+jadeFiles+".jade")
+    .pipe(duration('jade'))
     .pipe(jade(config).on('error', gutil.log))
     .pipe(gulpif(env === PRODUCTION && USE_FINGERPRINTING, fingerprint(jsManifest, { base:'assets/js/', prefix: 'assets/js/' })))
     //.pipe(gulpif(env === PRODUCTION && USE_FINGERPRINTING, fingerprint(vendorManifest, { base:'assets/js/', prefix: 'assets/js/' })))
     .pipe(gulpif(env === PRODUCTION && USE_FINGERPRINTING, fingerprint(cssManifest, { base:'assets/css/', prefix: 'assets/css/' })))
     .pipe(gulpif(env === PRODUCTION && USE_FINGERPRINTING, fingerprint(imagesManifest, { base:'assets/images/', prefix: 'assets/images/' })))
     .pipe(gulpif(env === PRODUCTION, size()))
-    .pipe(gulp.dest(getOutputDir()));
+    .pipe(gulp.dest(getOutputDir())).on('end', function() {
+      if (watching) livereload.changed('');
+    });
 });
-function myCoffee() {
-  var bundler = browserify({debug: env === DEVELOPMENT})
-    .add('./'+SRC+'/coffee/main.coffee');
 
-  return bundler.bundle()
-    .on('error', function(err) {
-      console.log(err.message);
-      this.end();
-    })
-    .pipe(source('main.js'))
-    .pipe(buffer())
-    .pipe(gulpif(env === PRODUCTION, uglify()))
-    .pipe(gulpif(env === PRODUCTION, size()))
-    .pipe(gulpif(env === PRODUCTION && USE_FINGERPRINTING, rev()))
-    .pipe(gulp.dest(getOutputDir()+ASSETS+'/js'))
-    .pipe(gulpif(env === PRODUCTION && USE_FINGERPRINTING, rev.manifest()))
-    .pipe(gulpif(env === PRODUCTION && USE_FINGERPRINTING, gulp.dest(BUILD+'/rev/js')))
-}
+
 gulp.task('coffee', function() {
+  function myCoffee() {
+    var bundler = browserify({debug: env === DEVELOPMENT})
+      .add('./'+SRC+'/coffee/main.coffee');
+
+    return bundler.bundle()
+      .on('error', function(err) {
+        console.log(err.message);
+        this.end();
+      })
+      .pipe(duration('coffee'))
+      .pipe(source('main.js'))
+      .pipe(buffer())
+      .pipe(gulpif(env === PRODUCTION, uglify()))
+      .pipe(gulpif(env === PRODUCTION, size()))
+      .pipe(gulpif(env === PRODUCTION && USE_FINGERPRINTING, rev()))
+      .pipe(gulp.dest(getOutputDir()+ASSETS+'/js'))
+      .pipe(gulpif(env === PRODUCTION && USE_FINGERPRINTING, rev.manifest()))
+      .pipe(gulpif(env === PRODUCTION && USE_FINGERPRINTING, gulp.dest(BUILD+'/rev/js')))
+  }
+
   gulp.src('./'+SRC+'/coffee/main.coffee')
     .pipe(plumber())
-    .pipe(myCoffee())
+    .pipe(myCoffee());
 });
 
 gulp.task('clean-js', function() {
@@ -117,8 +122,8 @@ gulp.task('vendor', function() {
     .pipe(gulpif(env === DEVELOPMENT, sourcemaps.init()))
     .pipe(concat('vendor.js'))
     .pipe(gulpif(env === DEVELOPMENT, sourcemaps.write()))
-    .pipe(gulpif(env === DEVELOPMENT, uglify({mangle:false})))
-    .pipe(size())
+    .pipe(gulpif(env === PRODUCTION, uglify({mangle:false})))
+    .pipe(gulpif(env === PRODUCTION, size()))
     .pipe(gulpif(env === PRODUCTION && USE_FINGERPRINTING, rev()))
     .pipe(gulp.dest(getOutputDir()+ASSETS+'/js'))
     .pipe(gulpif(env === PRODUCTION && USE_FINGERPRINTING, rev.manifest()))
@@ -128,26 +133,27 @@ gulp.task('vendor', function() {
 gulp.task('autoVariables', function() {
   return gulp.src(MOCKUPS+'/ai/autovariables.scss')
     .pipe(changed(SRC+'/sass'))
-    .pipe(gulp.dest(SRC+'/sass'));
+    .pipe(gulp.dest(SRC+'/sass'))
 });
 gulp.task('spriteSass', function() {
   return gulp.src(MOCKUPS+'/sprite/sprites.scss')
     .pipe(changed(SRC+'/sass'))
-    .pipe(gulp.dest(SRC+'/sass'));
+    .pipe(gulp.dest(SRC+'/sass'))
 });
-gulp.task('sass',['clean-css', 'autoVariables', 'spriteSass'], function() {
-  var imagesManifest = env === PRODUCTION && USE_FINGERPRINTING ? (JSON.parse(fs.readFileSync("./"+BUILD+'/rev/images/rev-manifest.json', "utf8"))) : {};
-  var config = {style: 'compact'};
+gulp.task('sass', function() {
+  var imagesManifest = env === PRODUCTION ? (JSON.parse(fs.readFileSync("./"+BUILD+'/rev/images/rev-manifest.json', "utf8"))) : {};
+  var config = { errLogToConsole: true };
 
   if (env === DEVELOPMENT) {
     config.sourceComments = 'map';
   } else if (env === PRODUCTION) {
-    config.sourcemap = false;
     config.outputStyle = 'compressed';
   }
   return gulp.src(SRC+'/sass/main.scss')
+    .pipe(duration('sass'))
+    .pipe(plumber())
     .pipe(sass(config).on('error', gutil.log))
-    .pipe(gulpif(!USE_SASS_MAPS, prefix("last 1 version", "> 1%", "ie 8", "ie 7")))
+    .pipe(gulpif(env === PRODUCTION, prefix("last 2 versions", "> 1%", "ie 8", "ie 7", { cascade: true })))
     .pipe(gulpif(env === PRODUCTION, csso()))
     .pipe(gulpif(env === PRODUCTION, size()))
     .pipe(gulpif(env === PRODUCTION && USE_FINGERPRINTING, fingerprint(imagesManifest, { base:'../images/', prefix: '../images/' })))
@@ -160,36 +166,48 @@ gulp.task('clean-css', function() {
   gulp.src(getOutputDir()+ASSETS+'/css', { read: false })
     .pipe(gulpif(env === PRODUCTION, clean()))
 });
-
-//gulp.task('images',['clean-images'], function() {
-//  return gulp.src([MOCKUPS+'/{images,sprite}/*.{jpg,png,gif}'])
-//    .pipe(imagemin({
-//      progressive: true
-//    }))
-//    .pipe(flatten())
-//    .pipe(gulpif(env === PRODUCTION, rev()))
-//    .pipe(gulp.dest(getOutputDir()+ASSETS+'/images'))
-//    .pipe(gulpif(env === PRODUCTION, rev.manifest()))
-//    .pipe(gulpif(env === PRODUCTION, gulp.dest(BUILD+'/rev/images')))
-//});
-//gulp.task('clean-images', function() {
-//  gulp.src(getOutputDir()+ASSETS+'/images', { read: false })
-//    .pipe(gulpif(env === PRODUCTION, clean()))
-//});
-
+gulp.task('images',['clean-images'], function() {
+  return gulp.src([MOCKUPS+'/{images,sprite}/**/*.{jpg,png,gif}'])
+    .pipe(imagemin({
+      progressive: true
+    }))
+    .pipe(duration('images'))
+    .pipe(flatten())
+    .pipe(gulpif(env === PRODUCTION && USE_FINGERPRINTING, rev()))
+    .pipe(gulp.dest(getOutputDir()+ASSETS+'/images'))
+    .pipe(gulpif(env === PRODUCTION && USE_FINGERPRINTING, rev.manifest()))
+    .pipe(gulpif(env === PRODUCTION && USE_FINGERPRINTING, gulp.dest(BUILD+'/rev/images')))
+});
+gulp.task('clean-images', function() {
+  gulp.src(getOutputDir()+ASSETS+'/images', { read: false })
+    .pipe(gulpif(env === PRODUCTION, clean()))
+});
 gulp.task('fonts', function() {
   return gulp.src(['node_modules/bootstrap/assets/fonts/**', MOCKUPS+"/fonts/*"])
-    .pipe(gulp.dest(getOutputDir()+ASSETS+'/fonts'));
+    .pipe(gulp.dest(getOutputDir()+ASSETS+'/fonts'))
 });
 
 gulp.task('watch', function() {
-  gulp.watch(SRC+'/**/*.{jade,svg,json}', ['jade']);
-  gulp.watch(SRC+'/**/*.js', ['coffee']);
-  gulp.watch(SRC+'/**/*.scss', ['sass']);
-  var server = livereload();
-  gulp.watch(BUILD+'**').on('change', function(file) {
-    server.changed(file.path);
+  watching = true;
+  livereload.listen();
+  gulp.watch(SRC+'/**/*.jade', ['jade']).on('error', gutil.log);
+  gulp.watch(SRC+'/**/*.{js,coffee}', ['coffee']).on('error', gutil.log);
+  gulp.watch(SRC+'/**/*.scss', ['sass']).on('error', gutil.log);
+  gulp.watch(BUILD+env+'/assets/**').on('change', function(file) {
+    console.log(file.path);
+    livereload.changed(file.path);
   }).on('error', gutil.log);
+});
+
+gulp.task('connect', function() {
+  useServer = true;
+  gulp.src(BUILD+env)
+    .pipe(webserver({
+      //host: '0.0.0.0',
+      livereload: true,
+      directoryListing: true,
+      open: "index.html"
+    }));
 });
 
 gulp.task('browserify-test', function() {
@@ -222,8 +240,25 @@ gulp.task('live', ['coffee', 'jade', 'sass', 'watch']);
 gulp.task('build', function() {
   runSequence(['fonts','images','spriteSass','autoVariables'],['fonts','coffee','sass'],['jade']);
 });
-
+gulp.task('server', ['connect', 'watch']);
 gulp.task('production', function() {
   env = PRODUCTION;
-  runSequence(['clean-js'],['fonts','coffee','sass'],['jade']);
+  runSequence(['images','clean-js'],['fonts','coffee','sass'],['jade']);
 });
+
+function sha1(buf) {
+  return crypto.createHash('sha1').update(buf).digest('hex');
+}
+
+function getFilesWithSha1(path) {
+  var a = [];
+  fs.readdirSync(path).map(function(file) {
+    var filePath = path+'/'+file;
+    var stats = fs.lstatSync(filePath);
+    if (stats.isFile()) {
+      var targetDigest = sha1(fs.readFileSync(filePath));
+      a.push(targetDigest)
+    }
+  });
+  return a;
+}
